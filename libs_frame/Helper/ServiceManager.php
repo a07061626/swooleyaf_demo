e5d62956f997401477f80ad83b573b788c612d95
jiangwei <jiangwei07061625@163.com>
2019-03-12 08:04:05 +0800

    git init

--- libs_frame/Helper/ServiceManager.php
+++ libs_frame/Helper/ServiceManager.php
@@ -17,6 +17,7 @@ class ServiceManager {
                 foreach ($projects as $eProject) {
                     foreach ($eProject['listens'] as $eListen) {
                         $command = $commandPrefix . ' -s start -module ' . $eProject['module_name'] . ' -port ' . $eListen['port'] . ' && ' . $commandPrefix . ' -s startstatus -module ' . $eProject['module_name'] . ' -port ' . $eListen['port'];
+                        echo $command;
                         system($command);
                     }
                 }
