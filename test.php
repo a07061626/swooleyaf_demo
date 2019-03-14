2f0180192dfe3d2472c492968711c83f0cba287c
jiangwei <jiangwei07061625@163.com>
2019-03-15 07:44:36 +0800

    init

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$server = new \swoole_websocket_server('172.18.134.124', 9501);
+$server = new \swoole_websocket_server('172.18.134.124', 7100);
 $server->on('message', function (\swoole_websocket_server $server, $frame) {
     $server->push($frame->fd, "this is server");
 });
