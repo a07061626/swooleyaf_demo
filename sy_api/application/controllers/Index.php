57e8a091d60ff59205173ddd5203c6aef0117d06
jiangwei <jiangwei07061625@163.com>
2019-03-14 08:56:48 +0800

    init

--- sy_api/application/controllers/Index.php
+++ sy_api/application/controllers/Index.php
@@ -1,12 +1,14 @@
 <?php
 class IndexController extends CommonController {
+    public $signStatus = true;
+
     public function init() {
         parent::init();
+        $this->signStatus = true;
     }
 
     /**
      * @SyFilter-{"field": "tag","explain": "标识","type": "string","rules": {"min": 1,"required":1}}
-     * @SyFilter-{"field": "_ignoresign","explain": "签名标识","type": "string","rules": {"min": 0}}
      */
     public function indexAction(){
         $this->SyResult->setData([
