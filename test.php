7b6b159c82cccfb2797e59bd1852ce92e23c80b6
jiangwei <jiangwei07061625@163.com>
2019-03-15 07:38:34 +0800

    init

--- test.php
+++ test.php
@@ -1,1 +1,12 @@
 <?php
+$server = new \swoole_websocket_server('172.18.134.124', 9501);
+$server->on('message', function (\swoole_websocket_server $server, $frame) {
+    $server->push($frame->fd, "this is server");
+});
+$server->on('request', function ($request, $response) {
+    $response->end("<h1>Hello Websocket Swoole. #" . random_int(1000, 9999) . "</h1>");
+});
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+$server->start();
